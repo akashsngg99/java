f58f1d01d2903718350e10f8a6685e796bde1fc5
Akash Singh <akash.sngg99@gmail.com>
2025-06-07 22:06:07 +0530

    Updated jdbc query

--- src/jdbc_conn.java
+++ src/jdbc_conn.java
@@ -1,12 +1,36 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class jdbc_conn {
     public static void main(String[] args) throws SQLException {
 
-        String ssql="select * from student";
-        Connection con= DriverManager.getConnection();
+        String ssql="select * from students";
+        String user="postgres";
+        String password="postgres";
+        String url="jdbc:postgresql://34.172.164.130:5432/postgres";
+
+       /* Connection con= DriverManager.getConnection(url,user,password);
+        Statement st= con.createStatement();
+        ResultSet rs= st.executeQuery(ssql);
+        System.out.println(rs);
+        rs.next();*/
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(ssql)) {
+
+            // Loop through each row of the result set
+            while (rs.next()) {
+                // Assuming you have columns named 'id' (integer) and 'name' (string)
+                int id = rs.getInt("student_id");
+                String name = rs.getString("first_name");
+                System.out.println("ID: " + id + ", Name: " + name);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
         
     }
 }
